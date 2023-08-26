4eb7e0f60c1e0d52dd7e203b4e3d2ce8dc1ff985
Kovalevvova <kovalev.vova2013@gmail.com>
2023-08-26 22:12:48 +0300

    111

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,11 @@
 class Header {
+  handlerOpenShoppingPage() {
+    shoppingPage.render();
+  }
   render(count) {
     const html = `
     <div class = "header-container">
-    <div class = "header-counter">🍕${count}</div>
+    <div class = "header-counter" onclick = "headerPage.handlerOpenShoppingPage();">🍕${count}</div>
     </div>
     `;
     ROOT_HEADER.innerHTML = html;
